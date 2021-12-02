35206da320570f2517cc184bb750d24c30cd5199
EZURA, Atsushi <ezura.atsushi@aainc.co.jp>
2021-12-02 16:53:56 +0900

    first test of git-flow workflow

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # git-flow sandbox for testing installation
+
+## test history
+- 2,Dec 2021 First test of git-flow
--- app/index.php
+++ app/index.php
@@ -2,5 +2,8 @@
 
 require_once 'Message/Message.php';
 
+// New
 $msg = new Message\Message('gif-flow sandgox');
+
+// Echo
 echo $msg->getMessage();
