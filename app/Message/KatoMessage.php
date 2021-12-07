de8d6293878915c3e2477a13b006e113e52f65f3
KatoTakayasu <72192104+KatoTakayasu@users.noreply.github.com>
2021-12-07 16:33:36 +0900

    kato test1

---
+++ app/Message/KatoMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Message;
+
+class Message
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+}
